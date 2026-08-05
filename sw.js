dae687ae41d09791664dca238e8c0393034a321f
wosatex <wosatex@googlemail.com>
2026-08-05 19:32:04 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    geht ungefiltert durch und wird nie aus dem Cache beantwortet. Eine
    gecachte Modellantwort waere schlimmer als gar keine. */
 
-const VERSION = "vectorizer-2026-08-05-c";
+const VERSION = "vectorizer-2026-08-05-d";
 const HUELLE = [
   "./",
   "./index.html",
